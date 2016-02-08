3e0e1050c7796c8113810c69c84234b0b4ee52dc
jtomaszk <jtomaszk@users.noreply.github.com>
2016-02-08 14:53:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,34 @@
+#!/usr/bin/python
+import sys
+
 from gumtreescraper import SearchListing
+from database import session, Base, engine
+from gumtree_item import GTItem
 
-searchListing = SearchListing("s-mieszkania-i-domy-sprzedam-i-kupie")
-# searchListing.do_search()
+Base.metadata.create_all(engine)
 
-print(searchListing.listing_results)
+searchListing = SearchListing(sys.argv[1], sys.argv[2], sys.argv[3])
+
+for result in searchListing.listing_results:
+    matched = session.query(GTItem).filter(GTItem.url == result.url).all()
+    if len(matched) == 0:
+        session.add(result)
+
+session.commit()
+
+from feedgen.feed import FeedGenerator
+fg = FeedGenerator()
+fg.title('Gumtree feed')
+fg.description('Gumtree feed')
+fg.link(href='http://example.com', rel='alternate')
+
+
+all_result = session.query(GTItem).order_by(GTItem.creation_date.desc()).limit(1000).all()
+
+for result in all_result:
+    fe = fg.add_entry()
+    fe.id('http://www.gumtree.pl/' + result.url)
+    fe.title(result.title)
+    fe.description(result.summary)
+
+fg.rss_file('rss.xml')
